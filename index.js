1635369ab10ffc21def974d79e9e380aea9b5dfd
HribernikBlaz <hrib.blazhrib@gmail.com>
2024-01-31 21:01:30 +0100

    added: background, new colors, feels like, visibility, logo

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 // index.js
-const apiKey = "327b51ff93b51989eb2214f90ad8b3e4";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const aK = "327b51ff93b51989eb2214f90ad8b3e4";
+const aU = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
-async function checkWeather(city) {
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
+async function checkWeather(city) {
+    const response = await fetch(aU + city + `&appid=${aK}`);
     if (response.status == 404) {
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
     } else {
         var data = await response.json();
-
+        console.log(data);
         document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+        document.querySelector(".feelsLike").innerHTML = Math.round(data.main.feels_like) + "°C";
+        document.querySelector(".visibility").innerHTML = (data.visibility)/1000 + " km";
 
         /*Update weather image*/
         if (data.weather[0].main == "Clouds") {
